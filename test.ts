2b5af37ac559490094757740884dff5be297756c
Niv Swisa LinearB <107345560+nivSwisa1@users.noreply.github.com>
2023-04-23 12:35:29 +0300

    test

--- test.ts
+++ test.ts
@@ -2,16 +2,15 @@
 //another
 // testing rules
 //another// testing rules
-//another 
+//another
 // testing rules
 //another
 // testing rules
 //another// testing rules
-//another 
+//another
+//another
 // testing rules
 //another
 // testing rules
-//another 
-// testing rules
 //another// testing rules
-//another 
+//another
